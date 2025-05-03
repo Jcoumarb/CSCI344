4ea9ebd15019f0f71bf27f5325010009ae9e9d4f
Jcoumarb <jitonri@gmail.com>
2025-05-02 23:16:30 -0400

    hw05 completed

--- homework/hw05/views/posts.py
+++ homework/hw05/views/posts.py
@@ -19,18 +19,55 @@ class PostListEndpoint(Resource):
 
     def get(self):
 
-        # giving you the beginnings of this code (as this one is a little tricky for beginners):
-        ids_for_me_and_my_friends = get_authorized_user_ids(self.current_user)
-        posts = Post.query.filter(Post.user_id.in_(ids_for_me_and_my_friends))
+        limit = request.args.get("limit")
+        if limit is None:
+            limit = 20
 
-        # TODO: add the ability to handle the "limit" query parameter:
+
+        #this checks for a valid limit
+        try:
+            limit = int(limit)
+        except:
+            return Response(json.dumps({"message": "Invalid integer"}), mimetype="application/json", status=400)
+
+        if limit > 50:
+            return Response(json.dumps({"message": "Invalid limit (n <= 50)"}), mimetype="application/json", status=400)
+
+        #this applies the limit
+        ids_for_me_and_my_friends = get_authorized_user_ids(self.current_user)
+        posts = (
+                Post.query
+                .filter(Post.user_id.in_(ids_for_me_and_my_friends))
+                .limit(limit)
+        )
 
         data = [item.to_dict(user=self.current_user) for item in posts.all()]
         return Response(json.dumps(data), mimetype="application/json", status=200)
 
     def post(self):
-        # TODO: handle POST logic
-        return Response(json.dumps({}), mimetype="application/json", status=201)
+
+        new_post = request.json
+
+        image = new_post.get("image_url")
+
+        if image is None:
+            return Response(
+                json.dumps({"message": "An image is required to make a post"}),
+                mimetype="application/json",
+                status=400
+            )
+
+        post = Post(
+            image_url=image,
+            user_id=self.current_user.id,
+            caption=data.data.get("caption"),
+            alt_text=data.get("alt_text"),
+        )
+
+        db.session.add(post)
+        db.session.commit()
+
+        return Response(json.dumps(post.to_dict(user=self.current_user)), mimetype="application/json", status=201)
 
 
 class PostDetailEndpoint(Resource):
@@ -40,24 +77,87 @@ class PostDetailEndpoint(Resource):
 
     def patch(self, id):
         print("POST id=", id)
-        # TODO: Add PATCH logic...
-        return Response(json.dumps({}), mimetype="application/json", status=200)
+        
+        data = request.json
+        post = Post.query.get(id)
+
+        # checks that post exists and is the current user's post
+        if post is None:
+            return Response(
+                json.dumps({"message": "Invalid post ID"}),
+                mimetype="application/json",
+                status=404,
+            )
+
+        if post.user_id != self.current_user.id:
+            return Response(
+                json.dumps({"message": "you are not authorized to edit this post"}),
+                mimetype="application/json",
+                status=404,
+            )
+
+        image = data.get("image_url")
+        caption = data.get("caption")
+        alt_text = data.get("alt_text")
+
+        if image is not None:
+            post.image_url = image
+
+        if caption is not None:
+            post.caption = caption
+
+        if alt_text is not None:
+            post.alt_text = alt_text
+
+        db.session.commit()
+
+        edited_post = Post.query.get(id)
+
+        return Response(json.dumps(edited_post.to_dict()), mimetype="application/json", status=200)
 
     def delete(self, id):
         print("POST id=", id)
 
-        # TODO: Add DELETE logic...
+        post = Post.query.get(id)
+
+        if post is None:
+            return Response(
+                json.dumps({"message": f"post id={id} not found"}),
+                mimetype="application/json",
+                status=404,
+            )
+
+        if post.user_id != self.current_user.id:
+            return Response(
+                json.dumps({"message": f"you are not authorized to get post id={id}"}),
+                mimetype="application/json",
+                status=404,
+            )
+
+        Post.query.filter_by(id=id).delete()
+        db.session.commit()
+
         return Response(
-            json.dumps({}),
+            json.dumps({"message":"post deleted"}),
             mimetype="application/json",
             status=200,
         )
 
     def get(self, id):
         print("POST id=", id)
-        # TODO: Add GET logic...
+        
+        post = Post.query.get(id)
+
+        #checks that post is exists before returning it
+        if post is None:
+            return Response(
+                json.dumps({"message": "Invalid post id"}),
+                mimetype="application/json",
+                status=404,
+            )
+
         return Response(
-            json.dumps({}),
+            json.dumps(post.to_dict()),
             mimetype="application/json",
             status=200,
         )
--- homework/hw05/views/profile.py
+++ homework/hw05/views/profile.py
@@ -14,9 +14,12 @@ class ProfileDetailEndpoint(Resource):
         self.current_user = current_user
 
     def get(self):
-        # TODO: Add GET logic...
+        
+        #gets current user profile
+        user = self.current_user
+
         return Response(
-            json.dumps({}),
+            json.dumps(user.to_dict()),
             mimetype="application/json",
             status=200,
         )
